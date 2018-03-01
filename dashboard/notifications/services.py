c6c84f6d58f3092ecd9b7fc8c6464f9ac25be466
Oliver Muellerklein <omuellerklein@berkeley.edu>
2018-02-28 17:32:36 -0800

    Fixed layout and added search bar to switch accounts

--- dashboard/notifications/services.py
+++ dashboard/notifications/services.py
@@ -3,19 +3,19 @@ import requests
 import os, os.path, json
 
 
-def get():
+def get(get_idx = 0):
     url = 'http://128.3.7.72:8000/accounts' 
     #params = {'count': count}
     #r = requests.get(url, params=params)
     r = requests.get(url)
 
     vpn = r.json()
-    acc_id = vpn['results'][0]['accountid']
-    acc_name = vpn['results'][0]['accountname']
-    acc_time = vpn['results'][0]['updated']
-    acc_alloc = vpn['results'][0]['accountallocation']
-    acc_balance = vpn['results'][0]['accountbalance']
-    acc_desc = vpn['results'][0]['description']
+    acc_id = vpn['results'][get_idx]['accountid']
+    acc_name = vpn['results'][get_idx]['accountname']
+    acc_time = vpn['results'][get_idx]['updated']
+    acc_alloc = vpn['results'][get_idx]['accountallocation']
+    acc_balance = vpn['results'][get_idx]['accountbalance']
+    acc_desc = vpn['results'][get_idx]['description']
     vpn_return = {'acc_id' : acc_id, 'acc_name' : acc_name, 
                 'acc_time' : acc_time, 'acc_desc' : acc_desc,
                 'acc_alloc' : acc_alloc, 'acc_balance' : acc_balance}
--- dashboard/notifications/views.py
+++ dashboard/notifications/views.py
@@ -3,31 +3,45 @@ from django.shortcuts import render, HttpResponse
 from django.contrib import messages
 from django import template 
 import os, os.path, json
+import requests
 
 # Create your views here.
-def notifications(request):
-	title = "Notification Center"
+def notifications(request):    	
 
-	if services.get():
-		messages.info(request, services.get())
-		serv_get = services.get()
+	if request.GET:
+		print("request.GET: ", request.GET)
+		req_val = request.GET['sample_form']
 
-	print("\nWithin notifications()")
-	print("services.get(): ", services.get())
-	print("-------\n")
+		url = 'http://128.3.7.72:8000/accounts' 
+		r = requests.get(url)
 
-	args = { 'the_title': title, 'acc_id': serv_get['acc_id'],
-				'acc_name': serv_get['acc_name'], 
-				'acc_time': serv_get['acc_time'],
-				'acc_desc': serv_get['acc_desc'],
-				'acc_alloc': serv_get['acc_alloc'],
-				'acc_balance': serv_get['acc_balance'] }
+		vpn = r.json()
+		#sample_acc = vpn['results'][int(req_val)]
+		serv_get = services.get(int(req_val))
+		title = "Notification Center"
+		args = { 'the_title': title, 'acc_id': serv_get['acc_id'],
+					'acc_name': serv_get['acc_name'], 
+					'acc_time': serv_get['acc_time'],
+					'acc_desc': serv_get['acc_desc'],
+					'acc_alloc': serv_get['acc_alloc'],
+					'acc_balance': serv_get['acc_balance'] }
+		return render(request, 'notifications.html', args)
+	else:
 
-	print("\nWithin notifications")
-	print("args: ", args)
-	print("+++++++\n\n")
-	
-	return render(request, 'notifications.html', args)
+		title = "Notification Center"
+
+		if services.get():
+			messages.info(request, services.get())
+			serv_get = services.get()
+
+		args = { 'the_title': title, 'acc_id': serv_get['acc_id'],
+					'acc_name': serv_get['acc_name'], 
+					'acc_time': serv_get['acc_time'],
+					'acc_desc': serv_get['acc_desc'],
+					'acc_alloc': serv_get['acc_alloc'],
+					'acc_balance': serv_get['acc_balance'] }
+		
+		return render(request, 'notifications.html', args)
 
 # Create class + methods for each JSON data field
 class getCount:
