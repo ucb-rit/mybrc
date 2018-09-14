2eeccdc90d89ae178bb4a6b2c5b1229a55cb2aeb
Benjamin Zhang <benzhang4@gmail.com>
2018-09-14 15:05:41 -0700

    Add custom permission class.

--- rest/rest/rest/settings.py
+++ rest/rest/rest/settings.py
@@ -112,7 +112,8 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
-    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated', 'api.permissions.UserOwnershipPermission']
 }
 
 # Custom Auth user model.
